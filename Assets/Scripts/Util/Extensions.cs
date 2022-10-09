0bfce33b0a8a3730636661d893cb044315e2b050
Jerri Ahonen <jerri.ahonen@gmail.com>
2022-10-09 14:37:12 +0300

    Add list extension

---
+++ Assets/Scripts/Util/Extensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Util
+{
+	public static class ListExtensions
+	{
+		public static T Random<T>(this List<T> self)
+		{
+			return self[UnityEngine.Random.Range(0, self.Count)];
+		}
+	}
+}

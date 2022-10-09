88b5372f48a724ad89489cd8ad7f9904e926237f
Jerri Ahonen <jerri.ahonen@gmail.com>
2022-10-09 18:59:11 +0300

    Actually add time manager

--- Assets/Scripts/TimeManager.cs
+++ Assets/Scripts/TimeManager.cs
@@ -1,7 +1,36 @@
-﻿namespace DefaultNamespace
+﻿
+using System;
+using UnityEngine;
+using UnityEngine.Rendering;
+using Util;
+
+public class TimeManager : Singleton<TimeManager>
 {
-	public class TimeManager
+	[SerializeField] private Light directionalLight;
+	[SerializeField] private float lightIntensity_DAY;
+	[SerializeField] private float lightIntensity_NIGHT;
+	
+	[SerializeField] private Volume dayVolume;
+	[SerializeField] private Volume nightVolume;
+
+	[SerializeField] private bool day;
+
+	private void OnValidate()
 	{
+		if (!directionalLight || !dayVolume || !nightVolume)
+			return;
 		
+		if (day)
+		{
+			directionalLight.intensity = lightIntensity_DAY;
+			dayVolume.weight = 1;
+			nightVolume.weight = 0;
+		}
+		else
+		{
+			directionalLight.intensity = lightIntensity_NIGHT;
+			dayVolume.weight = 0;
+			nightVolume.weight = 1;
+		}
 	}
 }

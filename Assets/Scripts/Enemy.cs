4850f43cb89e3161868af858650601d7708ba9ca
Jerri Ahonen <jerri.ahonen@gmail.com>
2022-10-09 17:56:10 +0300

    Rudimentary enemy ai

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,10 +1,16 @@
 using System;
+using Unity.AI.Navigation;
 using UnityEngine;
 using UnityEngine.AI;
+using Random = UnityEngine.Random;
+
+public enum EnemyState { Wander, Chase, RunAway }
 
 public class Enemy : MonoBehaviour
 {
 	private NavMeshAgent agent;
+	private NavMeshSurface navMesh;
+	private EnemyState state;
 
 	public event Action<Enemy> Captured;
 	
@@ -13,10 +19,78 @@ public class Enemy : MonoBehaviour
 		agent = GetComponent<NavMeshAgent>();
 	}
 
+	private Vector3 destination;
+	
+	private const float slowUpdateInterval = 2f;
+	private const float RunawayInterval = 5f;
+	private float elapsed;
 	private void Update()
 	{
 		var playerPos = WorldManager.I.Player.transform.position;
-		agent.destination = playerPos;
+		var dist = (playerPos - transform.position).sqrMagnitude;
+
+		if (WorldManager.I.Hunt)
+		{
+			Debug.Log("State: Chase");
+			state = EnemyState.Chase;
+		}
+		else if (dist > 14)
+		{
+			Debug.Log("State: Wander");
+			state = EnemyState.Wander;
+		}
+		else
+		{
+			Debug.Log("State: Run Away");
+			state = EnemyState.RunAway;
+		}
+
+		NavMeshHit hit;
+
+		switch (state)
+		{
+			case EnemyState.Wander:
+				if (elapsed > slowUpdateInterval)
+				{
+					var randomDirection = Random.insideUnitSphere * 28f;
+					randomDirection += transform.position;
+					NavMesh.SamplePosition(randomDirection, out hit, 28f, 1);
+					destination = hit.position;
+					agent.SetDestination(destination);
+					
+					elapsed = 0;
+				}
+				break;
+			case EnemyState.Chase:
+				if (dist < 10f)
+				{
+					destination = playerPos;
+					agent.SetDestination(destination);
+				}
+				else if (elapsed > slowUpdateInterval)
+				{
+					destination = playerPos;
+					agent.SetDestination(destination);
+					elapsed = 0;
+				}
+				break;
+			case EnemyState.RunAway:
+				if (elapsed > RunawayInterval)
+				{
+					var awayFromPlayer = transform.position - playerPos * 14f;
+					awayFromPlayer += transform.position;
+					NavMesh.SamplePosition(awayFromPlayer, out hit, 28f, 1);
+					destination = hit.position;
+					agent.SetDestination(destination);
+
+					elapsed = 0;
+				}
+				break;
+			default:
+				throw new ArgumentOutOfRangeException();
+		}
+
+		elapsed += Time.deltaTime;
 	}
 
 	private void OnTriggerEnter(Collider other)
@@ -33,5 +107,26 @@ public class Enemy : MonoBehaviour
 		Debug.Log("Enemy captured");
 		Captured?.Invoke(this);
 		Destroy(gameObject);
+	}
+
+	private void OnDrawGizmos()
+	{
+		switch (state)
+		{
+			case EnemyState.Wander:
+				Gizmos.color = Color.green;		
+				break;
+			case EnemyState.Chase:
+				Gizmos.color = Color.red;
+				break;
+			case EnemyState.RunAway:
+				Gizmos.color = Color.blue;
+				break;
+			default:
+				throw new ArgumentOutOfRangeException();
+		}
+		
+		Gizmos.DrawSphere(destination, 1f);
+		Gizmos.DrawRay(transform.position + Vector3.up, ((destination + Vector3.up) - transform.position).normalized * 10);
 	}
 }
--- Assets/Scripts/WorldManager.cs
+++ Assets/Scripts/WorldManager.cs
@@ -4,6 +4,8 @@ using Util;
 public class WorldManager : Singleton<WorldManager>
 {
 	[SerializeField] private Player player;
+	[SerializeField] private bool hunt;
 
 	public Player Player => player;
+	public bool Hunt => hunt;
 }
